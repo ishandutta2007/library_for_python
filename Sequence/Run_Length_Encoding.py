23702124a876267cc2604e5206e21db3b80eb10b
Kazun1998 <hkazu1998@gmail.com>
2025-07-27 21:16:37 +0900

    Alternating_Length_Encoding の実装

--- Sequence/Run_Length_Encoding.py
+++ Sequence/Run_Length_Encoding.py
@@ -15,3 +15,33 @@ def Run_Length_Encoding(S):
             R.append([S[i],1])
 
     return R
+
+def Alternating_Length_Encoding(S, first, second, equal = True) -> tuple[list[int], list[int]]:
+    """ アルファベットサイズが 2 である列 S に対して, 最初に現れる first と残りの second について, それぞれが連続して現れる回数のリストを求める.
+
+    Args:
+        S: 列
+        first: 最初に現れる文字
+        second: 次に現れる文字
+        equal (bool, optional): True にすると, 返り値における 2 つの配列の長さが等しくなる. Defaults to True.
+
+    Returns:
+        tuple[list[int], list[int]]: X, Y を整数のリストとしたタプル (X, Y). これは以下を意味する.
+            * S = (first が連続して X[0] 個)(second が連続して Y[0] 個)(first が連続して X[1] 個)(second が連続して Y[1] 個)...
+    """
+    x: list[int] = []
+    y: list[int] = []
+
+    if S[0] == second:
+        x.append(0)
+
+    for a, k in Run_Length_Encoding(S):
+        if a == first:
+            x.append(k)
+        else:
+            y.append(k)
+
+    if equal and len(x) > len(y):
+        y.append(0)
+
+    return x, y
